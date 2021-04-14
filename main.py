be0810f5f276d70ebbcf49e8c25bdacb40742baa
randomairborne <gxhutchinson@gmail.com>
2021-04-14 17:56:51 -0600

    Added some more stuff to help and main commands

--- main.py
+++ main.py
@@ -36,14 +36,13 @@ class Help(commands.MinimalHelpCommand):
             filtered = await self.filter_commands(commands, sort=True)
             command_signatures = [self.get_command_signature(c) for c in filtered]
             if command_signatures:
-                cog_name = getattr(cog, "qualified_name", "No Category")
+                cog_name = getattr(cog, "qualified_name", "System")
                 embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
-
-    async def send_error_message(self, error):
-        embed = discord.Embed(title="Error", value=error)
         channel = self.get_destination()
         await channel.send(embed=embed)
 
+    async def send_error_message(self, error):
+        embed = discord.Embed(title="Error", value=error)
         channel = self.get_destination()
         await channel.send(embed=embed)
 
