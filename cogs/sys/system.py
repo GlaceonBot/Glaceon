084fa6a1a19ac92083bc839277e1ca1d9fd25262
randomairborne <gxhutchinson@gmail.com>
2021-04-15 21:30:52 -0600

    Added message on server join!

--- cogs/sys/system.py
+++ cogs/sys/system.py
@@ -1,10 +1,21 @@
 import pathlib
 import sqlite3
+
 import discord
 from discord.ext import commands
 
 path = pathlib.PurePath()
 embedcolor = 0xadd8e6
+
+
+async def sendwelcome(channel: discord.TextChannel):
+    embed = discord.Embed(colour=embedcolor, title="Hi!")
+    embed.add_field(name="Thank you for adding me to your server!",
+                    value="I appreciate your interest in Glaceon! Run `%help` to get started!\n"
+                          "Make sure to read the [TOS](https://randomairborne.dev/glaceon/tos)"
+                          " and the [privacy policy](https://randomairborne.dev/glaceon/privacy).",
+                    inline=False)
+    await channel.send(embed=embed)
 
 
 class BotSystem(commands.Cog):
@@ -48,6 +59,35 @@ class BotSystem(commands.Cog):
         modmailchannel.commit()
         modmailchannel.close()
         await ctx.send(f"ModMail channel is now {channel}")
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, ctx):
+        sent = False
+        for name in ctx.channels:
+            if not sent:
+                if name.name == "bot-spam":
+                    await sendwelcome(name)
+                    sent = True
+                elif name.name == "bot-commands":
+                    await sendwelcome(name)
+                    sent = True
+                elif name.name == "bot":
+                    await sendwelcome(name)
+                    sent = True
+                elif name.name == "off-topic":
+                    await sendwelcome(name)
+                    sent = True
+                elif name.name == "general":
+                    await sendwelcome(name)
+                    sent = True
+                else:
+                    for text_channels in ctx.channels:
+                        if sent is False:
+                            try:
+                                await sendwelcome(text_channels)
+                                sent = True
+                            except AttributeError:
+                                pass
 
 
 def setup(glaceon):
