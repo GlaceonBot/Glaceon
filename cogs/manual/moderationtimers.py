8799918bb2a254b654976cb717204a539e07d104
randomairborne <gxhutchinson@gmail.com>
2021-05-18 19:16:50 -0600

    Add startup for unbanner/muter

--- cogs/manual/moderationtimers.py
+++ cogs/manual/moderationtimers.py
@@ -1,5 +1,6 @@
 from discord.ext import tasks, commands
 
+
 class UnCog(commands.Cog):
     def __init__(self, bot):
         self.index = 0
@@ -10,3 +11,12 @@ class UnCog(commands.Cog):
 
     @tasks.loop(seconds=5.0)
     async def unbanner(self):
+        pass
+
+    @tasks.loop(seconds=5.0)
+    async def unmuter(self):
+        pass
+
+
+def setup(glaceon):
+    glaceon.add_cog(UnCog(glaceon))
