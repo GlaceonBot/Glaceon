497d0e8cf76997411986fc83f0fbc8a159fe2dbf
randomairborne <gxhutchinson@gmail.com>
2021-06-21 21:52:40 -0600

    add info command

--- cogs/sys/info.py
+++ cogs/sys/info.py
@@ -1,4 +1,8 @@
 import pathlib
+import cpuinfo
+import shutil
+import psutil
+import platform
 import discord
 import requests
 from discord.ext import commands
@@ -40,6 +44,23 @@ class Info(commands.Cog):
             name="**__You can invite Glaceon to your server with the link below__**",
             value=f"**[Invite](https://glaceon.xyz/invite)**",
             inline=True)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def info(self, ctx):
+        embed = discord.Embed(colour=self.glaceon.embedcolor, title="System Info")
+        embed.set_footer(text=f"Request by {ctx.author}")
+        cpu_info = platform.uname().processor.split(" ")
+        embed.add_field(name="CPU", value=f"{cpuinfo.get_cpu_info()['brand_raw']}\n{psutil.cpu_count()}-thread {int(psutil.cpu_freq().current)} MHz {cpuinfo.get_cpu_info()['arch']}", inline=True)
+        embed.add_field(name="RAM", value=str(round(psutil.virtual_memory().total / 1073741824)) + "GiB", inline=True)
+        embed.add_field(name="OS", value=f"{platform.system()} {platform.release()}", inline=True)
+        embed.add_field(name="Python version", value=platform.python_version(), inline=True)
+        embed.add_field(name="Discord.py version", value=discord.__version__, inline=True)
+        embed.add_field(name="Glaceon version", value="0.0.1", inline=True)
+        embed.add_field(name="CPU usage", value=str(psutil.cpu_percent()) + "%", inline=True)
+        embed.add_field(name="RAM usage", value=f"{psutil.virtual_memory().percent}%, {int(psutil.virtual_memory().used / 1048576)}MiB used, {int(psutil.virtual_memory().free / 1048576)} MiB free", inline=True)
+        embed.add_field(name="Disk usage", value=f"{int(shutil.disk_usage('.').total / 1073741824)}GiB total, {int(shutil.disk_usage('.').used / 1048576)} MiB used, {int(shutil.disk_usage('.').free / 1048576)} MiB free", inline=True)
         await ctx.send(embed=embed)
 
 
