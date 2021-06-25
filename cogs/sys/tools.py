39dec746ff787ed3a9ec07e0ac63267b8b266698
valkyrie_pilot <69158910+randomairborne@users.noreply.github.com>
2021-06-25 00:43:32 -0600

    not hit the discord char limit

--- cogs/sys/tools.py
+++ cogs/sys/tools.py
@@ -9,12 +9,22 @@ class Tools(commands.Cog):
   @commands.command(aliases=['exec', 'eval', 'ssh', 'rsh', 'sh'])
   @commands.is_owner()
   async def shell(self, ctx, *args):
+    maxmsglength = 3994
     process = subprocess.run(args, capture_output=True)
+    stdout = process.stdout.decode('utf8')
+    stderr = proccess.stderr.decode('utf8')
     if process.returncode == 0:
-      output_discord_ified = f"stdout: ```{process.stdout.decode('utf8')}```\n exitcode: `{process.returncode}`\n command: `{' '.join(process.args)}` "
+      stdout_chunks = [stdout[i:i + maxmsglength] for i in range(0, len(stdout), maxmsglength)]
+        for stdout_part in stdout_chunks:
+            await ctx.send("```\n" + stdout_part + "\n```")
+        await ctx.send(f"Exit code: {process.returncode}\n Command: {' '.join(process.args)}")
     else:
-      output_discord_ified = f"stderr: ```{process.stderr.decode('utf8')}```\n exitcode: `{process.returncode}`\n command: `{' '.join(process.args)}` "
-    await ctx.send(output_discord_ified)
+      stderr_chunks = [stderr[i:i + maxmsglength] for i in range(0, len(stderr), maxmsglength)]
+        for stderr_part in stderr_chunks:
+            await ctx.send("```\n" + stderr_part + "\n```")
+        await ctx.send(f"Exit code: {process.returncode}\n Command: {' '.join(process.args)}")
+    for output_discord_ified in outputs:
+      await ctx.send(output_discord_ified)
     
 
 def setup(glaceon):
