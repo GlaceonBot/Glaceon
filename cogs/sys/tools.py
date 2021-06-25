07b7a8e9ccba25a7426f006f30bd2763567cafb7
valkyrie_pilot <69158910+randomairborne@users.noreply.github.com>
2021-06-25 00:13:28 -0600

    add ssh command

---
+++ cogs/sys/tools.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class Tools(commands.Cog):
+  def __init__(self, glaceon):
+    self.glaceon = glaceon
+  
+  @commands.command(aliases=['exec', 'eval', 'ssh', 'rsh', 'sh'])
+  @commands.is_owner()
+  async def shell(self, ctx, *args):
+    process = subprocess.run([args, capture_output=True)
+    await ctx.send(f"stdout: ```{process.stdout}```,\n stderr: ```{process.stderr}```, exitcode: `{subprocess.returncode}`")
+    
+
+def setup(glaceon):
+    glaceon.add_cog(BotSystem(glaceon))
