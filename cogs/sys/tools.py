a9d2751f558c7462858a53841f26553abb41b399
valkyrie_pilot <69158910+randomairborne@users.noreply.github.com>
2021-06-25 00:32:55 -0600

    fix strange stdout behavior

--- cogs/sys/tools.py
+++ cogs/sys/tools.py
@@ -10,7 +10,11 @@ class Tools(commands.Cog):
   @commands.is_owner()
   async def shell(self, ctx, *args):
     process = subprocess.run(args, capture_output=True)
-    await ctx.send(f"stdout: ```{process.stdout.decode('utf8')}```\n stderr: ```{process.stderr.decode('utf8')}```\n exitcode: `{process.returncode}`\n command: `{' '.join(process.args)}` ")
+    if process.returncode == 0:
+      output_discord_ified = f"stdout: ```{process.stdout.decode('utf8')}```\n exitcode: `{process.returncode}`\n command: `{' '.join(process.args)}` "
+    else:
+      output_discord_ified = f"stderr: ```{process.stderr.decode('utf8')}```\n exitcode: `{process.returncode}`\n command: `{' '.join(process.args)}` "
+    await ctx.send(output_discord_ified)
     
 
 def setup(glaceon):
