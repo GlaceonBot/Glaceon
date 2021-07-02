29a3b4a08912abc99ac3a896850056640e481413
Grant Hutchinson <gxhutchinson@gmail.com>
2021-07-02 20:52:28 +0000

    modified start command

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/Glaceon/Glaceon
-./venv/bin/python3 -m pip install -r requirements.txt
-webhook -hooks glaceon-reloader-webhook.json -verbose > glaceon.log 2>&1 &
+./venv/bin/python3 -m pip install -r requirements.txt > glaceon.log 2>&1
 ./main.py > glaceon.log 2>&1
