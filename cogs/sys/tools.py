5274b3fd6acf01b3628446d5fd210480236bdc31
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-07-02 18:18:52 -0700

    Remove shlex import, switch to proc variable completely (I think)

--- cogs/sys/tools.py
+++ cogs/sys/tools.py
@@ -2,7 +2,6 @@ import subprocess
 import asyncio
 import os
 import discord
-import shlex
 from discord.ext import commands
 
 class Tools(commands.Cog):
@@ -14,6 +13,7 @@ class Tools(commands.Cog):
   async def shell(self, ctx, *, args):
     """This command is used to execute shell commands on Glaceon's host server. Only <@!788222689126776832> and <@!545463550802395146> can use it."""
     MAX_MSG_LENGTH = 1988
+    
     proc = await asyncio.create_subprocess_shell(
         args,
         stdout=asyncio.subprocess.PIPE,
@@ -23,16 +23,17 @@ class Tools(commands.Cog):
     
     stdout = stdout.decode('utf-8')
     stderr = stderr.decode('utf-8')
-    if process.returncode == 0:
+
+    if proc.returncode == 0:
       stdout_chunks = [stdout[i:i + MAX_MSG_LENGTH] for i in range(0, len(stdout), MAX_MSG_LENGTH)]
       for stdout_part in stdout_chunks:
        await ctx.send("```\n" + stdout_part + "\n```")
-      await ctx.send(f"Exit code: {process.returncode}\nCommand: {args.replace('bash -c ', ' ', 1)}")
+      await ctx.send(f"Exit code: {proc.returncode}\nCommand: {args}")
     else:
       stderr_chunks = [stderr[i:i + MAX_MSG_LENGTH] for i in range(0, len(stderr), MAX_MSG_LENGTH)]
       for stderr_part in stderr_chunks:
        await ctx.send("```\n" + stderr_part + "\n```")
-      await ctx.send(f"Exit code: {process.returncode}\nCommand: {args.replace('bash -c ', ' ', 1)}")
+      await ctx.send(f"Exit code: {proc.returncode}\nCommand: {args}")
     
   @commands.command()
   @commands.is_owner()
