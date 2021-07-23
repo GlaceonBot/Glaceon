cd805cc1c1078f0ea2181a7203b5fa0f93ab8711
valkyrie_pilot <valk@randomairborne.dev>
2021-07-23 14:31:33 -0500

    python, not python3

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/Glaceon/Glaceon
-./venv/bin/python3 -m pip install -r requirements.txt > glaceon.log 2>&1
+./venv/bin/python -m pip install -r requirements.txt > glaceon.log 2>&1
 ./main.py --loglevel=info
