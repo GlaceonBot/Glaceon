4e70c8f5c71f2f209d3faf17bd2b76997b715707
valkyrie_pilot <valk@randomairborne.dev>
2021-08-26 16:28:03 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/Glaceon
-./venv/bin/python -m pip install -U -r requirements.txt > glaceon.log 2>&1
+./venv/bin/pip install -U -r requirements.txt > glaceon.log 2>&1
 ./main.py --loglevel=info
